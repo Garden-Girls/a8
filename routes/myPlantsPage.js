ac46874f17a424a3e84a5938024f60e4338291c0
Skylar Yi Li <yil067@ucsd.edu>
2021-03-13 20:44:36 +0800

    styling

--- routes/myPlantsPage.js
+++ routes/myPlantsPage.js
@@ -9,23 +9,12 @@ exports.view = function(req, res){
 
 var myPlantsData = require("../myPlantsData.json")
 
-	var startDate = new Date(myPlantsData.startDate);
-	
-	var dateCurr = new Date();
-
-	var plantAge = dateCurr.getTime() - startDate.getTime();
-	var ageDays = plantAge/(1000 * 3600 * 24);
-	ageDays = Math.floor(ageDays);
-	
-
-  
 
   res.render('myPlantsPage', {
   		"img": myPlantsData.species,
   		"nickname": myPlantsData.nickname,
 		"species": myPlantsData.species,
 		"Plants": myPlantsData.Plants,
-		"autoAge": ageDays,
 		"watering": myPlantsData.watering
   });
 
